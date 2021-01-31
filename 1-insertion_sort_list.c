b1edf57312a35f3ae2beab9437133cdd66838886
Emmastro <emurairi18@alastudents.org>
2021-01-31 21:37:50 +0100

    First commit

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+
+/**
+ * insertion_sort_list - insertion sort algorithm
+ * @array: pointer to array to sort
+*/
+void insertion_sort_list(listint_t **list)
+{
+
+short int swapped;
+int tmp;
+
+do {
+	swapped = 0;
+	for (i = 1; i < size; i++)
+	{
+		if (array[i - 1] > array[i])
+		{
+			tmp = array[i];
+			array[i] = array[i - 1];
+			array[i - 1] = tmp;
+			swapped = i;
+			print_array(array, len);
+		}
+	}
+	size = swapped;
+} while (size >= 1);
+
+}
+
