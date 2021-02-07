50dc59980026f5ef626f0cc5be7dd2177fb4b0c8
Emmastro <emurairi18@alastudents.org>
2021-02-07 23:20:16 +0400

    Fix quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 /**
  * quick_sort - sorts an array of integers in ascending order
  * using the quick sort sort algorithm
@@ -17,14 +18,14 @@ recursive_quick_sort(array, size, 0, size - 1);
  * @start: start index
  * @end: end index
 */
-void recursive_quick_sort(int *array, size_t size, size_t start, size_t end)
+void recursive_quick_sort(int *array, size_t size, int start, int end)
 {
-size_t p;
+int p;
 if (start < end)
 {
 	p = partition(array, size, start, end);
-	if (p != 0)
-		recursive_quick_sort(array, size, start, p - 1);
+
+	recursive_quick_sort(array, size, start, p - 1);
 	recursive_quick_sort(array, size, p + 1, end);
 }
 
@@ -38,32 +39,32 @@ if (start < end)
  * @end: end index
  * Return: partition index
 */
-size_t partition(int *array, size_t size, size_t start, size_t end)
+size_t partition(int *array, size_t size, int start, int end)
 {
 int pivot = array[end];
-size_t i = start;
-size_t j;
+int i = start;
+int j;
 
-for (j = i + 1; j <= end; j++)
+for (j = i; j <= end - 1; j++)
 {
 	if (array[j] < pivot)
 	{
-		swap_int(array, i, j);
-		print_array(array, size);
+		swap_int1(array, i, j);
 		i++;
 	}
 }
-swap_int(array, i, end);
+swap_int1(array, i, end);
+print_array(array, size);
 return (i);
 }
 
 /**
- * swap_int - swap variable values
+ * swap_int1 - swap variable values
  * @array: array to use
  * @a: index 1
  * @b: index 2
 */
-void swap_int(int *array, size_t a, size_t b)
+void swap_int1(int *array, int a, int b)
 {
 int tmp;
 tmp = array[a];
--- sort.h
+++ sort.h
@@ -20,8 +20,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap_int(int *array, size_t a, size_t b);
-void recursive_quick_sort(int *array, size_t size, size_t start, size_t end);
-size_t partition(int *array, size_t size, size_t start, size_t end);
+void swap_int1(int *array, int a, int b);
+void recursive_quick_sort(int *array, size_t size, int start, int end);
+size_t partition(int *array, size_t size, int start, int end);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
